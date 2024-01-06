b8715bc0626717b2862e5ca95882acd4cd468316
Thisisfake777 <131435888+Thisisfake777@users.noreply.github.com>
2024-01-06 16:04:33 +0530

    Add files via upload

---
+++ InstagramLogin/src/main/java/com/demo/testSpring/InstagramLoginApplication.java
@@ -0,0 +1,13 @@
+package com.demo.testSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramLoginApplication.class, args);
+	}
+
+}
---
+++ InstagramLogin/src/test/java/com/demo/testSpring/InstagramLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.testSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstagramLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
